4804959fcffa0e9fa8b23acab2c71633009948c5
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-06-19 16:01:33 -0400

    first commit

---
+++ CloudUnlock/CloudUnlock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CloudUnlock
+//
+//  Created by Mulkey, Rodrigo S. on 6/19/16.
+//  Copyright © 2016 Mulkey, Rodrigo S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
